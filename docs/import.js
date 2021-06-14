e2549a89e156c930575c803c29cd953ee5d91e9a
jinyang <961284688@qq.com>
2021-06-14 22:38:30 +0800

    docs(import): 告知使用getDeepPresetMergeAndModify的原因

--- docs/import.js
+++ docs/import.js
@@ -1,6 +1,7 @@
 // jest.config.js
 
 const { PresetUtils } = require('@ads/node-utils');
+// 强化jest的presets功能
 module.exports = PresetUtils.getDeepPresetMergeAndModify({
     presets: [
         require('@ads/jest-config-node')({
