62d022bbc23ab7cf1f42bec880d1fcce40d01cd8
jinyang <961284688@qq.com>
2021-06-14 22:42:46 +0800

    docs: 将@ads/node-utils加入依赖

--- ads.doc.config.js
+++ ads.doc.config.js
@@ -2,7 +2,6 @@
  * @Author: 锦阳
  * @Create: 2021年05月31日
  */
-const pkg = require('./package.json');
 const GenDoc = require('@ads/cli-plugin-doc');
 const fs = require('fs-extra');
 const { FastPath } = require('@ads/node-utils');
@@ -19,7 +18,7 @@ module.exports = (
         return {
             output: 'README.md',
             helpers: {
-                installCode: `npm i -D ${pkg.name} @ads/node-utils`,
+                devInstall: true,
                 importCode: fs.readFileSync(FastPath.getCwdPath('./docs/import.js'), { encoding: 'utf-8' }),
                 remark: fs.readFileSync(FastPath.getCwdPath('./docs/usage.md'), { encoding: 'utf-8' }),
                 postfixes: [
